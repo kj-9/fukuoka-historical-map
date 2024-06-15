79e9e133be24cf3014a877a3bd50a160006401d7
Kj <46800811+kj-9@users.noreply.github.com>
2024-06-15 22:49:25 +0900

    feat: Add CogLayer component for displaying COG (Cloud-Optimized GeoTIFF) data on the map

--- src/components/MapApp.svelte
+++ src/components/MapApp.svelte
@@ -1,6 +1,7 @@
 <script>
     import Map from "./ol/Map.svelte";
     import BaseLayer from "./ol/BaseLayer.svelte";
+    import CogLayer from "./ol/CogLayer.svelte";
 
 
     const CENTER = [130.398, 33.591]; // fukuoka
@@ -8,4 +9,5 @@
 
 <Map center={CENTER} zoom={12} minZoom={2} maxZoom={18}>
   <BaseLayer />
+  <CogLayer url="https://pub-032bef0ea1394491b7b31e5e413aa462.r2.dev/fukuoka.cog.tif" opacity={0.2} />
 </Map>
---
+++ src/components/ol/CogLayer.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+    import WebGLTile from "ol/layer/WebGLTile";
+    import GeoTIFF from "ol/source/GeoTIFF";
+
+    import { getContextMap } from "./context.svelte.ts";
+    
+    
+    let { url, zIndex = 0, visible = true, opacity = 1, loading = false } = $props();
+    let map = getContextMap();
+    
+
+    const cogSource = new GeoTIFF({
+      sources: [
+        {
+          url: url,
+        },
+      ],
+    });
+  
+    const cogLayer = new WebGLTile({
+      source: cogSource,
+      visible,
+      zIndex,
+      opacity,
+    });
+    
+    $effect(() => {
+      map.instance?.addLayer(cogLayer);
+    });
+  
+  </script>
+  
