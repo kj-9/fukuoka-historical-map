690f740fec9d01a7c9b95dd7cffad0f75fadca15
Kj <46800811+kj-9@users.noreply.github.com>
2024-06-15 22:35:49 +0900

    feat: Update MapApp to use specific center coordinates for Fukuoka

--- src/components/MapApp.svelte
+++ src/components/MapApp.svelte
@@ -2,9 +2,10 @@
     import Map from "./ol/Map.svelte";
     import BaseLayer from "./ol/BaseLayer.svelte";
 
+
+    const CENTER = [130.398, 33.591]; // fukuoka
 </script>
 
-
-<Map center={[0, 0]} zoom={2}>
-    <BaseLayer />
+<Map center={CENTER} zoom={12} minZoom={2} maxZoom={18}>
+  <BaseLayer />
 </Map>
