ed4d1abf71ae1d3ca60dd269fdfc829cfda651fc
Kj <46800811+kj-9@users.noreply.github.com>
2024-06-19 00:55:55 +0900

    feat: Add RotateControl component for map rotation functionality

---
+++ src/components/ol/RotateControl.svelte
@@ -0,0 +1,48 @@
+<script>
+  import Rotate from "ol/control/Rotate";
+  import { getContextMap } from "./context.svelte.ts";
+  import { easeOut } from "ol/easing.js";
+
+  let toggle = true;
+  let duration = 250;
+
+  let map = getContextMap();
+  const initailRotation = map.instance.getView().getRotation();
+  console.debug("initial rotation", initailRotation);
+
+  function resetNorthOrInitialRotation() {
+    const view = map.instance.getView();
+    if (!view) {
+      // the map does not have a view, so we can't act
+      // upon it
+      return;
+    }
+
+    const rotation = view.getRotation();
+    if (rotation !== undefined) {
+      if (duration > 0) {
+        view.animate({
+          rotation: toggle ? 0 : initailRotation, // north or initial rotation
+          duration,
+          easing: easeOut,
+        });
+        toggle = !toggle;
+      } else {
+        view.setRotation(0);
+      }
+    }
+  }
+
+  let transform = "";
+  map.instance.getView().on(["change:rotation"], (e) => {
+    const rotation = e.target.getRotation();
+    transform = "rotate(" + rotation + "rad)";
+  });
+</script>
+
+<button
+  class="btn btn-sm btn-square text-lg"
+  onclick={resetNorthOrInitialRotation}
+>
+  <div style="transform: {transform}">⇧</div>
+</button>
