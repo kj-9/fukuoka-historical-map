8d2bb0970a8b600911724db569aaaee36c0872c0
Kj <46800811+kj-9@users.noreply.github.com>
2024-06-19 00:56:28 +0900

    chore: Update ol/Map.svelte to include Attribution control and fix import formatting

--- src/components/ol/Map.svelte
+++ src/components/ol/Map.svelte
@@ -1,45 +1,43 @@
 <script>
-    import "ol/ol.css";
-    import Map from "ol/Map";
-    import { useGeographic } from "ol/proj";
-    import View from "ol/View";
-    import Attribution from "ol/control/Attribution";
-　　 import Rotate from 'ol/control/Rotate.js';
-    import {
-        DragRotateAndZoom,
-        defaults as defaultInteractions,
-    } from 'ol/interaction.js';
+  import "ol/ol.css";
+  import Map from "ol/Map";
+  import { useGeographic } from "ol/proj";
+  import View from "ol/View";
+  import Attribution from "ol/control/Attribution"; //import Rotate from 'ol/control/Rotate.js';
+  import {
+    DragRotateAndZoom,
+    defaults as defaultInteractions,
+  } from "ol/interaction.js";
 
-    import {initContextMap} from "./context.svelte.ts"
+  import { initContextMap } from "./context.svelte.ts";
 
-    let { center, zoom, minZoom = 0, maxZoom =28 } = $props();
-    let map = initContextMap();
+  let { center, rotation, zoom, minZoom = 0, maxZoom = 28 } = $props();
+  let map = initContextMap();
 
-    const mapId = "olmap";
+  const mapId = "olmap";
 
-    $effect(() => {
+  $effect(() => {
     useGeographic();
     const instance = new Map({
-        target: mapId,
-        controls: [new Attribution({}), new Rotate()],
-        interactions: defaultInteractions().extend([new DragRotateAndZoom()]),
-        view: new View({
+      target: mapId,
+      controls: [new Attribution({})],
+      interactions: defaultInteractions().extend([new DragRotateAndZoom()]),
+      view: new View({
         center,
         zoom,
         minZoom,
         maxZoom,
+        rotation,
         //extent: [103.130035, 0.822573, 104.801331, 1.724593],
-        }),
+      }),
     });
 
     map.instance = instance;
-    });
+  });
 </script>
 
-
 <div id={mapId} class="h-screen">
-    {#if map?.instance}
-        <slot />
-    {/if}
+  {#if map?.instance}
+    <slot />
+  {/if}
 </div>
-  
