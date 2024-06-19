6fbd050ea260652e3e23dc14b180facdf64abc90
Kj <46800811+kj-9@users.noreply.github.com>
2024-06-19 20:14:26 +0900

    feat: Add lazy loading for images in MapApp.svelte

--- src/components/MapApp.svelte
+++ src/components/MapApp.svelte
@@ -3,19 +3,29 @@
   import BaseLayer from "./ol/layer/BaseLayer.svelte";
   import CogControl from "./ol/CogControl.svelte";
   import RotateControl from "./ol/RotateControl.svelte";
+  import SetViewControl from "./ol/SetViewControl.svelte";
 
-  const CENTER = [130.398, 33.591]; // fukuoka
+  const CENTER = [130.4030020104645, 33.591078733935646]; // fukuoka
+
   const ROTATION = 0.6788023617718635;
   const COG_URL =
     "https://pub-032bef0ea1394491b7b31e5e413aa462.r2.dev/fukuoka.cog.tif";
 
   const INITIAL_OPACITY = 0.8;
+  const INITIAL_ZOOM = 13;
 </script>
 
-<Map center={CENTER} rotation={ROTATION} zoom={12} minZoom={2} maxZoom={18}>
+<Map
+  center={CENTER}
+  rotation={ROTATION}
+  zoom={INITIAL_ZOOM}
+  minZoom={2}
+  maxZoom={18}
+>
   <!-- use sm:bottom-auto to cancel bottom-x -->
   <div class="z-10 absolute bottom-5 left-5 sm:top-5 sm:bottom-auto">
     <div class="flex items-center gap-3">
+      <SetViewControl center={CENTER} rotation={ROTATION} zoom={INITIAL_ZOOM} />
       <RotateControl autoHide={true} />
       <CogControl url={COG_URL} initialOpacity={INITIAL_OPACITY} />
     </div>
