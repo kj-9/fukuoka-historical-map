182085a53c4d3e87bb9304786e9b092802963355
Kj <46800811+kj-9@users.noreply.github.com>
2024-06-19 20:14:21 +0900

    feat: Add SetViewControl component for setting the view center in OpenLayers

---
+++ src/components/ol/SetViewControl.svelte
@@ -0,0 +1,15 @@
+<script>
+  import { getContextMap } from "./context.svelte.ts";
+
+  let { center } = $props();
+
+  let map = getContextMap();
+
+  function onclick() {
+    console.debug("setting view");
+
+    map.instance.getView().setCenter(center);
+  }
+</script>
+
+<button class="btn btn-sm btn-square" {onclick}> [ ] </button>
